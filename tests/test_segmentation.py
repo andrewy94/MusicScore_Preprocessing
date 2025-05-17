7ee28824075a6b079a46d64a10f384194b358fbc
andrewy94 <122841883+andrewy94@users.noreply.github.com>
2025-05-16 20:53:20 -0600

    added test case for connected_component_segmentation

--- tests/test_segmentation.py
+++ tests/test_segmentation.py
@@ -2,7 +2,7 @@ import pytest
 import cv2
 import numpy as np
 from tests.utils import generate_dummy_score, verify_bboxes
-from pipeline.segmentation import find_segments, full_segmentation
+from pipeline.segmentation import find_segments, projection_segmentation, connected_component_segmentation
 
 # baseline test
 def test_find_segments_one_pixel():
@@ -58,7 +58,7 @@ def test_find_segments_eight_staves():
     assert len(vert_segment_coords) == 1, f"Expected 1 segment, but got {len(vert_segment_coords)} segments "
 
 # simple image
-def test_full_segmentation_low_depth():
+def test_projection_segmentation_low_depth():
     # setup
     dummy_img = np.zeros((1000, 1000), dtype = np.uint8)
     dummy_img[500, 500] = 255 # middle dot
@@ -78,12 +78,12 @@ def test_full_segmentation_low_depth():
                   (800, 250, 801, 751), 
                   (250, 800, 751, 801)]
     
-    act_bboxes = full_segmentation(dummy_img)
+    act_bboxes = projection_segmentation(dummy_img)
 
     assert act_bboxes == exp_bboxes, f"Expected bboxes {exp_bboxes}, but got {act_bboxes}"
 
 # complex image   
-def test_full_segmentation_moderate_depth():
+def test_projection_segmentation_moderate_depth():
     # setup
     dummy_img = np.zeros((1000, 1000), dtype = np.uint8)
     dots = [
@@ -95,6 +95,7 @@ def test_full_segmentation_moderate_depth():
         (650, 540),
         (100, 100),
         (900, 100),
+        (650, 675),
         (500, 505)
     ]
     lines = [
@@ -106,6 +107,7 @@ def test_full_segmentation_moderate_depth():
         ((700, 300), (600, 400)),
         ((300, 700), (400, 600)),
         ((700, 700), (600, 600)),
+        ((650, 675), (650, 725))
     ]
 
     for x, y in dots:
@@ -115,24 +117,61 @@ def test_full_segmentation_moderate_depth():
         cv2.line(dummy_img, start, end, 255, thickness = 1)
 
     # test
-    exp_bboxes = [(100, 100, 101, 101), 
-                  (200, 200, 201, 201), 
-                  (250, 200, 751, 201), 
-                  (800, 200, 801, 201), 
-                  (200, 250, 201, 751), 
-                  (300, 300, 401, 401), 
-                  (300, 600, 401, 701), 
-                  (500, 500, 501, 501), 
-                  (505, 500, 506, 501), 
-                  (540, 650, 541, 651), 
-                  (600, 300, 701, 401), 
-                  (600, 600, 701, 701), 
-                  (800, 250, 801, 751), 
-                  (200, 800, 201, 801), 
-                  (250, 800, 751, 801), 
-                  (800, 800, 801, 801), 
+    exp_bboxes = [(100, 100, 101, 101), (200, 200, 201, 201), (250, 200, 751, 201), 
+                  (800, 200, 801, 201), (200, 250, 201, 751), (300, 300, 401, 401), 
+                  (300, 600, 401, 701), (500, 500, 501, 501), (505, 500, 506, 501), 
+                  (540, 650, 541, 651), (600, 300, 701, 401), (600, 600, 701, 726), 
+                  (800, 250, 801, 751), (200, 800, 201, 801), (250, 800, 751, 801), 
+                  (800, 800, 801, 801), (100, 900, 101, 901)]
+    
+    act_bboxes = projection_segmentation(dummy_img)
+
+    # cv2.imshow("img", verify_bboxes(dummy_img, act_bboxes))
+    # cv2.waitKey(0)
+    assert act_bboxes == exp_bboxes, f"Expected bboxes {exp_bboxes}, but got {act_bboxes}"
+
+def test_connected_component_segmentation():
+    # setup
+    dummy_img = np.zeros((1000, 1000), dtype = np.uint8)
+    dots = [
+        (500, 500),
+        (200, 200),
+        (800, 200),
+        (200, 800),
+        (800, 800),
+        (650, 540),
+        (100, 100),
+        (900, 100),
+        (650, 675),
+        (500, 505)
+    ]
+    lines = [
+        ((200, 250), (200, 750)),
+        ((250, 200), (750, 200)),
+        ((800, 250), (800, 750)),
+        ((750, 800), (250, 800)),
+        ((300, 300), (400, 400)),
+        ((700, 300), (600, 400)),
+        ((300, 700), (400, 600)),
+        ((700, 700), (600, 600)),
+        ((650, 675), (650, 725))
+    ]
+
+    for x, y in dots:
+        dummy_img[x, y] = 255
+
+    for start, end in lines:
+        cv2.line(dummy_img, start, end, 255, thickness = 1)
+
+    # test
+    exp_bboxes = [(100, 100, 101, 101), (200, 200, 201, 201), (250, 200, 751, 201), 
+                  (800, 200, 801, 201), (200, 250, 201, 751), (800, 250, 801, 751), 
+                  (300, 300, 401, 401), (600, 300, 701, 401), (500, 500, 501, 501), 
+                  (505, 500, 506, 501), (300, 600, 401, 701), (600, 600, 701, 701), 
+                  (540, 650, 541, 651), (675, 650, 676, 651), (650, 675, 651, 726), 
+                  (200, 800, 201, 801), (250, 800, 751, 801), (800, 800, 801, 801), 
                   (100, 900, 101, 901)]
     
-    act_bboxes = full_segmentation(dummy_img)
+    act_bboxes = connected_component_segmentation(dummy_img)
 
     assert act_bboxes == exp_bboxes, f"Expected bboxes {exp_bboxes}, but got {act_bboxes}"
